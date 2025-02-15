c9ce52007035a79a949a40bc50c208257b2e23a1
suvanshenoy <suvanshenoy155@gmail.com>
2025-02-15 23:39:29 +0530

    format no-argumets-error.mjs using 'biome format'

--- kcc/errors/no-arguments-error.mjs
+++ kcc/errors/no-arguments-error.mjs
@@ -1,11 +1,10 @@
 class NoArgumentsError extends Error {
-  constructor(message, code = "NO_ARGS_PROVIDED", details = null) {
-    super(message);
-    this.name = "NoArgumentsError";
-    this.code = code;
-    this.details = details;
-  }
+	constructor(message, code = "NO_ARGS_PROVIDED", details = null) {
+		super(message);
+		this.name = "NoArgumentsError";
+		this.code = code;
+		this.details = details;
+	}
 }
 
 export default NoArgumentsError;
-
