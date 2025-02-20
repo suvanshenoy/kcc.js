ce56473732a5381cfcc256763eed20aad43e0e0e
suvanshenoy <suvanshenoy155@gmail.com>
2025-02-21 01:31:21 +0530

    Remove throw new ArgumentsError: - Replace it with logStatus

--- kcc/bin/kcc.mjs
+++ kcc/bin/kcc.mjs
@@ -1,9 +1,10 @@
-import processKittyTomlConfig from "../utils/process-kitty-toml-config.mjs";
 import process from "node:process";
 import { Command } from "commander";
-import NoArgumentsError from "../errors/no-arguments-error.mjs";
 import { existsSync } from "node:fs";
 import path from "node:path";
+
+import processKittyTomlConfig from "../utils/process-kitty-toml-config.mjs";
+import { logStatus } from "../utils/helpers/log-status.mjs";
 
 const program = new Command();
 
@@ -16,11 +17,15 @@ program
 program.parse(process.argv);
 
 if (!process.argv.slice(2).length) {
-	throw new NoArgumentsError(
+	logStatus(
+		"[status:fail]",
 		"No arguments provided. Please pass at least one option",
-		"NO_ARGS_PROVIDED",
-		{ hint: "Try runing `kcc --help` or `kcc -h` for usage information." },
 	);
+	logStatus(
+		"[status:hint]",
+		"Try running `kcc --help` or `kcc -h` for usage information.",
+	);
+	process.exit(1);
 }
 
 const options = program.opts();
@@ -28,9 +33,11 @@ const options = program.opts();
 if (options.file) {
 	const filePath = path.resolve(options.file);
 	if (!existsSync(filePath)) {
-		throw new Error(
+		logStatus(
+			"[status:fail]",
 			`File '${options.file}' does not exist in '${process.cwd()}'.`,
 		);
+		process.exit(1);
 	}
 	processKittyTomlConfig(process.cwd());
 }
