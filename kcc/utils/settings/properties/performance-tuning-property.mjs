aeb3512d8b13ad38ba5434f322ad0125e523d332
suvanshenoy <suvanshenoy155@gmail.com>
2025-08-19 20:51:51 +0530

    Update the conflicting names in properties directory

--- kcc/utils/settings/properties/performance-tuning-property.mjs
+++ kcc/utils/settings/properties/performance-tuning-property.mjs
@@ -1,4 +1,4 @@
-export const performanceTuning = {
+export const performanceTuningProperty = {
 	repaint_delay: "performance_tunning.repaint_delay",
 	input_delay: "performance_tunning.input_delay",
 	sync_to_monitor: "performance_tunning.sync_to_monitor",
